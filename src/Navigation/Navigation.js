927dee0a48ef44a840e847afdd2fde761efbcfa3
Shridutt Patel <shridutt270@gmail.com>
2020-07-24 13:56:46 +0530

    Initial commit

---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { AppBar, Tabs, Tab, Typography, Box } from '@material-ui/core'
+class Navigation extends Component {
+ 
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+export default Navigation;
