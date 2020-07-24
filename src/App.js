d3cc8f04c8fb4230018e1192940687e4d4fef5e6
Shridutt Patel <shridutt270@gmail.com>
2020-07-24 15:57:48 +0530

    after implementing the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Navigation from './Navigation/Navigation';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        material-UI
+        <Navigation />
       </div>
     );
   }
--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-import { AppBar, Tabs, Tab, Typography, Box } from '@material-ui/core'
+import { AppBar, Tabs, Tab, Typography, Box } from '@material-ui/core';
+import NavTab from './NavTab/NavTab';
+import classes from './Navigation.css'
 class Navigation extends Component {
- 
+    state= {
+        value : 0
+    }
+    handleChange = (event, newValue) => {
+        this.setState({value : newValue});
+    }
     render() {
         return (
-            <div></div>
+            <div >
+                <AppBar variant="elevation" position="fixed">
+                    <Tabs value={this.state.value} onChange={this.handleChange} aria-label="simple tabs example">
+                            <Tab variant="" label="Managers"></Tab>
+                            <Tab label="Your Employees"></Tab>
+                            <Tab label="New Employees/Manager"></Tab>
+                            <Tab style={{marginLeft: 'auto'}} label='Logout'></Tab>
+                    </Tabs>
+                </AppBar>
+            </div>
         );
     }
 }
