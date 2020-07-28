c055cb090520922f2d6007b46e883fa09f549e19
Shridutt Patel <shridutt270@gmail.com>
2020-07-28 15:17:16 +0530

    refectoring

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppBar, Tabs, Tab, Typography, Box } from '@material-ui/core';
+import { AppBar, Tabs, Tab } from '@material-ui/core';
 import { Route, Switch, Redirect } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import NewUser from '../containers/NewUsers/NewUsers';
@@ -7,22 +7,36 @@ import AllUsers from '../containers/AllUsers/AllUsers';
 import YourUser from '../containers/YourUsers/YourUsers';
 import classes from './Navigation.css';
 class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value : 0
+        }
+        if (window.location.href.includes('/Managers')) {
+            this.state.value = 0;
+        } else if (window.location.href.includes('/your-employees')) {
+            this.state.value = 1;
+        } else if (window.location.href.includes('/new-user')) {
+            this.state.value = 2;
+        }
+        console.log(window.location.href)
+    }
     state= {
-        value : 0
+        value : null
     }
     handleChange = (event, newValue) => {
         this.setState({value : newValue});
     }
     render() {
         let navBar = (
-            <AppBar variant="outlined" position="fixed">
+            <AppBar variant="elevation" position="fixed">
                     <Tabs value={this.state.value} onChange={this.handleChange} aria-label="simple tabs example">
                             <Tab  style={{textTransform : 'capitalize'}} component={Link} to="/Managers" label="Managers"></Tab>
                             <Tab style={{textTransform : 'capitalize'}} component={Link} to="/your-employees" label="Your Employees"></Tab>
                             <Tab style={{textTransform : 'capitalize'}} component={Link} to="/new-user" label="New user"></Tab>
                             <Tab  style={{marginLeft: 'auto',textTransform : 'capitalize'}} label='Logout'></Tab>
                     </Tabs>
-                </AppBar>
+            </AppBar>
         );
         
         let routes = (
--- src/containers/YourUsers/YourUsers.js
+++ src/containers/YourUsers/YourUsers.js
@@ -1,10 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { withStyles, Grid, Paper } from "@material-ui/core";
+const styles = {
+    root : {
+        display : 'flex',
+        justifyContent : 'center',
+    },
+  paper: {
+    height: "80vh",
+    marginTop: "4rem",
+    padding: "2rem",
+    width: "70%",
+    /* position : 'relative',
+    left : '10%' */
+    
+  },
+  GridContainer : {
+      height : '90vh'
+  }
+};
 class YourUsers extends Component {
- 
-    render() {
-        return (
-            <div></div>
-        );
-    }
+  render() {
+    const { classes } = this.props;
+    return (
+        <Grid container direction="row" justify="center" alignItems="center">
+          <Grid  item sm={4}>
+            <Paper className={classes.paper} elevation={3}>
+              Employees
+            </Paper>
+          </Grid>
+          <Grid item sm={4}>
+            <Paper className={classes.paper} elevation={3}>
+              Selected Employee
+            </Paper>
+          </Grid>
+        </Grid>
+    );
+  }
 }
-export default YourUsers;
+export default withStyles(styles)(YourUsers);
