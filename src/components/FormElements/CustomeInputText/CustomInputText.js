c9e01712a1dc4fae65864e89b544cb985a4154b8
shridutt <shridutt270@gmail.com>
2020-08-08 21:12:49 +0530

    solved passwordIcon issue when invalid

--- src/components/FormElements/CustomeInputText/CustomInputText.js
+++ src/components/FormElements/CustomeInputText/CustomInputText.js
@@ -1,18 +1,39 @@
 /* eslint-disable react/jsx-no-duplicate-props */
 import React from "react";
-import classes from './inouttext.css'
-import { TextField } from '@material-ui/core';
+import classes from "./inouttext.css";
+import { TextField } from "@material-ui/core";
 import { useField } from "formik";
-import { Visibility } from '@material-ui/icons'
+import { Visibility, VisibilityOff } from "@material-ui/icons";
 const CustomeInputText = (props) => {
-    const [field, meta] = useField(props);
-    const errorText = meta.error && meta.touched ? meta.error : '';
-    const { visiblilitytoggler } =props
+  const [field, meta] = useField(props);
+  const errorText = meta.error && meta.touched ? meta.error : "";
+  const { visiblilitytoggler } = props;
   return (
-      <div className={classes.Input}>
-        <TextField  autoComplete="off"  type={props.type} name={props.name}  helperText={errorText} style={{width : '20rem'}}  variant="outlined" label={props.label} error={meta.error && meta.touched} {...field}/>
-        {props.visibilityicon === 'true'?<div onClick={visiblilitytoggler} className={classes.Icon}><Visibility color='primary'/></div> : null}
-      </div>
+    <div className={classes.Input}>
+      <TextField
+        autoComplete="off"
+        type={props.type}
+        name={props.name}
+        helperText={errorText}
+        style={{ width: "20rem" }}
+        variant="outlined"
+        label={props.label}
+        error={meta.error && meta.touched}
+        {...field}
+      />
+      {props.visibilityicon === "true" ? (
+        <div
+          onClick={visiblilitytoggler}
+          className={
+            errorText.length === 0
+              ? classes.Icon
+              : [classes.Icon, classes.IconInvalid].join(" ")
+          }
+        >
+          { props.type === 'password' ? <Visibility color="primary" /> : <VisibilityOff color='primary'/>}
+        </div>
+      ) : null}
+    </div>
   );
 };
 export default CustomeInputText;
